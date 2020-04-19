5c8c61e8f6173c47c5438383b65ab88ee0f510dd
Saju Paul <saju.christel@blu-pa.com>
2020-04-19 17:18:31 +0530

    Adding static and dynamic router

---
+++ src/pages/[vehicle]/[person].js
@@ -0,0 +1,9 @@
+import {useRouter} from 'next/router'
+export default function person() {
+
+const router= useRouter();
+console.log(router.query);
+
+
+return <h1>{router.query.person}'s {router.query.vehicle}</h1>
+}
